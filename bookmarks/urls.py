a65b58e423fc4a22f11daeef8ac9abed0d807d25
travis88 <hellsballs99@gmail.com>
2017-01-07 12:02:31 +0300

    Images add download

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('account.urls')),
+    # url(r'^social-auth/', include('social.apps.django_app.urls', namespace='social')),
+    # url(r'^social-auth/', include('social_auth.urls', namespace='social')),
+    url(r'^images/', include('images.urls', namespace='images')),
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
