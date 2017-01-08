3ea5b5d3eb4b50e5a09b756d4fe401f4c8f07c74
travis88 <hellsballs99@gmail.com>
2017-01-08 14:07:51 +0300

    Add followers

--- account/urls.py
+++ account/urls.py
@@ -17,12 +17,16 @@ urlpatterns = [
 	url(r'^password-change/$', password_change, name='password_change'),
 	url(r'^password-change/done/$', password_change_done, name='password_change_done'),
 	# restore password urls
-	url(r'password-reset/$', password_reset, name='password_reset'),
-	url(r'password-reset/done/$', password_reset_done, name='password_reset_done'),
-	url(r'password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', password_reset_confirm, name='password_reset_confirm'),
-	url(r'password-reset/complete/$', password_reset_complete, name='password_reset_complete'),
+	url(r'^password-reset/$', password_reset, name='password_reset'),
+	url(r'^password-reset/done/$', password_reset_done, name='password_reset_done'),
+	url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', password_reset_confirm, name='password_reset_confirm'),
+	url(r'^password-reset/complete/$', password_reset_complete, name='password_reset_complete'),
 	# registration urls
-	url(r'register/$', views.register, name='register'),
+	url(r'^register/$', views.register, name='register'),
 	# edit profile urls
-	url(r'edit/$', views.edit, name='edit'),
+	url(r'^edit/$', views.edit, name='edit'),
+	# list and detail urls
+	url(r'^users/$', views.user_list, name='user_list'),
+	url(r'^users/follow/$', views.user_follow, name='user_follow'),
+	url(r'^users/(?P<username>[-\w]+)/$', views.user_detail, name='user_detail'),
 ]
